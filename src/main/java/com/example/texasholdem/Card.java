9ca4baffab6f50fea3df2a5981ef489671c28507
Sungeheon <elon4856@gmail.com>
2021-10-13 16:05:18 +0900

    add: 카드 종류를 생성하세요.

---
+++ src/main/java/com/example/texasholdem/Card.java
@@ -0,0 +1,7 @@
+package com.example.texasholdem;
+
+public enum Card { Spade_A,Spade_2,Spade_3,Spade_4,Spade_5,Spade_6,Spade_7,Spade_8,Spade_9,Spade_10, Spade_J, Spade_Q,Spade_K,
+    Clover_A,Clover_2,Clover_3,Clover_4,Clover_5,Clover_6,Clover_7,Clover_8,Clover_9,Clover_10,Clover_j,Clover_Q,Clover_K,
+    Hart_A,Hart_2,Hart_3,Hart_4,Hart_5,Hart_6,Hart_7,Hart_8,Hart_9,Hart_10,Hart_J,Hart_Q,Hart_K,
+    Diamond_A,Diamond_2,Diamond_3,Diamond_4,Diamond_5,Diamond_6,Diamond_7,Diamond_8,Diamond_9,Diamond_10,Diamond_j,Diamond_Q,Diamond_K
+}
