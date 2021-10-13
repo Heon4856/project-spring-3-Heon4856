7f94864e43575a615493521ae43c7d5c057491ea
Sungeheon <elon4856@gmail.com>
2021-10-13 19:39:11 +0900

    Refactoring:카드 객체를 리팩토링 하세요. 카드 한장 한장 만듬 --> 규칙을 통해서 생성.

--- src/main/java/com/example/texasholdem/Card.java
+++ src/main/java/com/example/texasholdem/Card.java
@@ -1,7 +1,22 @@
 package com.example.texasholdem;
 
-public enum Card { Spade_A,Spade_2,Spade_3,Spade_4,Spade_5,Spade_6,Spade_7,Spade_8,Spade_9,Spade_10, Spade_J, Spade_Q,Spade_K,
-    Clover_A,Clover_2,Clover_3,Clover_4,Clover_5,Clover_6,Clover_7,Clover_8,Clover_9,Clover_10,Clover_j,Clover_Q,Clover_K,
-    Hart_A,Hart_2,Hart_3,Hart_4,Hart_5,Hart_6,Hart_7,Hart_8,Hart_9,Hart_10,Hart_J,Hart_Q,Hart_K,
-    Diamond_A,Diamond_2,Diamond_3,Diamond_4,Diamond_5,Diamond_6,Diamond_7,Diamond_8,Diamond_9,Diamond_10,Diamond_j,Diamond_Q,Diamond_K
+public class Card {
+
+    public enum Rank { TWO, THREE, FOUR, FIVE, SIX,
+        SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE };
+
+    public enum Suit { CLOVER, DIAMOND, HEART, SPADE }
+
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank rank() { return rank; }
+    public Suit suit() { return suit; }
+    public String toString() { return rank + " of " + suit; }
+
 }
