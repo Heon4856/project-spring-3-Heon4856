becce762e76fd29c990347a89d89e9124f755f8b
Sungeheon <elon4856@gmail.com>
2021-10-13 20:54:17 +0900

    feat: 카드를 섞는 메소드와 카드를 뽑는 메소드를 추가하세요.

--- src/main/java/com/example/texasholdem/CardDeck.java
+++ src/main/java/com/example/texasholdem/CardDeck.java
@@ -1,25 +1,37 @@
 package com.example.texasholdem;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 실제 게임에 플레이할 카드 뭉치.
  */
 public class CardDeck {
-    private static final List<Card> protoDeck = new ArrayList<>();
+    private static final List<Card> cardDeck = new ArrayList<>();
 
     static {
         for (Card.Suit suit : Card.Suit.values())
             for (Card.Denomination denomination : Card.Denomination.values())
-                protoDeck.add(new Card(denomination, suit));
+                cardDeck.add(new Card(denomination, suit));
+    }
+
+    public List<Card> shuffle(){
+        Collections.shuffle((cardDeck));
+        return cardDeck;
+    }
+
+    public Card draw(){
+        Card drawnCard = cardDeck.get(0);
+        cardDeck.remove(0);
+        return drawnCard;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        for(Card card : protoDeck){
+        for(Card card : cardDeck){
             sb.append(card.toString());
             sb.append("\n");
         }
