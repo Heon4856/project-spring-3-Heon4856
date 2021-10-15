3454b902b38566e4b212fba59964c2de906a29bb
Sungeheon <elon4856@gmail.com>
2021-10-15 16:42:24 +0900

    feat: putCardToTable 메소드와 burnCard메소드를 추가하세요.

--- src/main/java/com/example/texasholdem/Dealer.java
+++ src/main/java/com/example/texasholdem/Dealer.java
@@ -3,15 +3,23 @@ package com.example.texasholdem;
 public class Dealer {
     private CardDeck cardDeck;
 
-    public Dealer( CardDeck cardDeck){
+    public Dealer(CardDeck cardDeck) {
         this.cardDeck = cardDeck;
 
     }
 
 
-    public void putCard(Player player, CardDeck cardDeck){
+    public void putCardToPlayer(Player player, CardDeck cardDeck) {
         player.getCard(cardDeck.draw());
     }
 
+    public void putCardToTable(Table table, CardDeck cardDeck) {
+        table.getCard(cardDeck.draw());
+
+    }
+
+    public void burnCard(CardDeck cardDeck) {
+        cardDeck.draw();
+    }
 
 }
