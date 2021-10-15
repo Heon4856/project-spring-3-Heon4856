3417ad3da32b9e9ed829bf8cb2d3db23af940609
Sungeheon <elon4856@gmail.com>
2021-10-15 16:16:02 +0900

    fix: player 생성시 카드패도 생성하세요. 없을 시 NullPointerException일어남.

--- src/main/java/com/example/texasholdem/Player.java
+++ src/main/java/com/example/texasholdem/Player.java
@@ -1,31 +1,33 @@
 package com.example.texasholdem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
 
     private String name;
     private Long chips;
-    private List<Card> PlayerCard;
+    private List<Card> playerCard;
     private boolean turn;
     private Long bet;
 
-    public Player(String name,Long chips){
-        this.name=name;
+    public Player(String name, Long chips) {
+        this.name = name;
         this.chips = chips;
+        this.playerCard = new ArrayList<>();
     }
 
     public void setBet(Long bet) {
         this.bet = bet;
     }
 
-    public void getCard(Card card){
-        PlayerCard.add(card);
+    public void getCard(Card card) {
+        playerCard.add(card);
     }
 
 
-    public List<Card> showCards(){
-        return PlayerCard;
+    public List<Card> showCards() {
+        return playerCard;
     }
 
 
