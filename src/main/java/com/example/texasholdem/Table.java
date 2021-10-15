f9567910ae5620cd5aaffe29ebf1d81354e6d79a
Sungeheon <elon4856@gmail.com>
2021-10-15 17:28:37 +0900

    feat: 테이블에 베팅머니를 추가하는 기능과, 베팅된 금액의 크기를 보여주는 기능을 추가하세요.

--- src/main/java/com/example/texasholdem/Table.java
+++ src/main/java/com/example/texasholdem/Table.java
@@ -5,12 +5,27 @@ import java.util.List;
 
 public class Table {
     private List<Card> tableCard;
+    private Long pot;
 
     public Table() {
         this.tableCard = new ArrayList<>();
+        this.pot = 0L;
     }
 
     public void getCard(Card card) {
         tableCard.add(card);
     }
+
+    public List<Card> showCards() {
+        return tableCard;
+    }
+
+    public void moneyToPot(Long betMoney){
+        this.pot = this.pot + betMoney;
+    }
+
+    public Long getPot() {
+        return pot;
+    }
+
 }
