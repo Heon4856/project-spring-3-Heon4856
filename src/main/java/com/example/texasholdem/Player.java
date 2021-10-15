28d8e8924dc779e54b62835531b5f80b2cc89161
Sungeheon <elon4856@gmail.com>
2021-10-16 02:17:41 +0900

    feat: player 클래스 변수들을 불러오는 메서드들을 작성하세요.

--- src/main/java/com/example/texasholdem/Player.java
+++ src/main/java/com/example/texasholdem/Player.java
@@ -8,23 +8,30 @@ public class Player {
     private String name;
     private Long chips;
     private List<Card> playerCard;
-    private boolean turn;
+    private boolean live;
 
     public Player(String name, Long chips) {
         this.name = name;
         this.chips = chips;
         this.playerCard = new ArrayList<>();
+        this.live = true;
     }
 
     public void Bet(Long CashToBet) {
         this.chips = this.chips - CashToBet;
+    }
 
+    public boolean isLive() {
+        return this.live;
+    }
+
+    public void Die() {
+        this.live = false;
     }
 
     public void getCard(Card card) {
         playerCard.add(card);
     }
-
 
     public List<Card> showCards() {
         return playerCard;
@@ -32,6 +39,19 @@ public class Player {
 
     public Long getChips(){
         return chips;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", chips=" + chips +
+                ", cards=" + playerCard +
+                '}';
+    }
+
+    public String getName(){
+        return this.name;
     }
 
 
