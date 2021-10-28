0c9240531d9c292c88d8d83cb4786c854dfd7013
Sungeheon <elon4856@gmail.com>
2021-10-28 16:15:31 +0900

    feat: 스트레이트를 찾는 메소드를 추가하세요.

--- src/main/java/com/example/texasholdem/HandValueEvaluator.java
+++ src/main/java/com/example/texasholdem/HandValueEvaluator.java
@@ -1,9 +1,10 @@
 package com.example.texasholdem;
 
+import com.example.texasholdem.Card.Denomination;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import com.example.texasholdem.Card.Denomination;
 
 public class HandValueEvaluator {
 
@@ -13,14 +14,13 @@ public class HandValueEvaluator {
     Denomination denomination;
     Integer straightCheckNum;
 
-    public String HandEvaluator(List<Card> allCard ){
+    public String HandEvaluator(List<Card> allCard) {
 
-        straightCheckNum =findStraight(allCard);
+        straightCheckNum = findStraight(allCard);
 
-        if (straightCheckNum>0 ){
-
-            return straightCheckNum+"straight";
-        }else{
+        if (straightCheckNum > 0) {
+            return straightCheckNum + "straight";
+        } else {
             return "not Straight";
         }
 
@@ -28,28 +28,26 @@ public class HandValueEvaluator {
 
     private int findStraight(List<Card> allCard) {
         List<String> denominationList = Card.getDenominationList();
-
-        for(Card card : allCard){
-            System.out.println("card.getDenomination() = " + card.getDenomination());
-            Integer cardOrder=denominationList.indexOf(card.getDenomination());
-            straightCheck.add(cardOrder);
-        }
-
-        straightCheck.sort(Comparator.naturalOrder());
         int countValue = 0;
         int straightCheckNum = 0;
-        for( int i=0 ; i<straightCheck.size()-1; i++){
 
+        for (Card card : allCard) {
+            Integer cardOrder = denominationList.indexOf(card.getDenomination());
+            straightCheck.add(cardOrder);
+        }
+        straightCheck.sort(Comparator.naturalOrder());
 
-            if( straightCheck.get(i+1)-straightCheck.get(i) ==1 ){
+        for (int i = 0; i < straightCheck.size() - 1; i++) {
+            if (straightCheck.get(i + 1) - straightCheck.get(i) == 1) {
                 countValue += 1;
-                if(countValue >5){
-                    straightCheckNum = straightCheck.get(i+1);
+                if (countValue > 3) {
+                    straightCheckNum = straightCheck.get(i + 1);
+                } else if (countValue == 3 && straightCheck.get(6) == 12) {
+                    straightCheckNum = straightCheck.get(i + 1);
                 }
-            } else{
-                countValue = 0 ;
+            } else {
+                countValue = 0;
             }
-
         }
         return straightCheckNum;
 
