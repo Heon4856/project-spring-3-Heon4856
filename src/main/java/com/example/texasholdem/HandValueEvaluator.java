2a1fbf059075ab394648ba7b917ac1e13a93cdbc
Sungeheon <elon4856@gmail.com>
2021-10-28 18:24:43 +0900

    feat: flush를 확인하는 메소드를 구현하세요.

--- src/main/java/com/example/texasholdem/HandValueEvaluator.java
+++ src/main/java/com/example/texasholdem/HandValueEvaluator.java
@@ -13,10 +13,16 @@ public class HandValueEvaluator {
     List<Integer> straightCheck = new ArrayList<>();
     Denomination denomination;
     Integer straightCheckNum;
+    String flushCheckString;
 
     public String HandEvaluator(List<Card> allCard) {
 
+        flushCheckString=findFlush(allCard);
         straightCheckNum = findStraight(allCard);
+
+        if (flushCheckString != "nope"){
+            return flushCheckString;
+        }
 
         if (straightCheckNum > 0) {
             return straightCheckNum + "straight";
@@ -25,6 +31,60 @@ public class HandValueEvaluator {
         }
 
     }
+
+    //todo :플러시 중 제일 큰 수 찾기.
+    private String findFlush(List<Card> allCard) {
+        List<String> denominationList = Card.getDenominationList();
+        List<Integer> cloverCards = new ArrayList<>();
+        List<Integer> spadeCards = new ArrayList<>();
+        List<Integer> diamondCards = new ArrayList<>();
+        List<Integer> heartCards = new ArrayList<>();
+
+        int cloverCount = 0;
+        int spadeCount = 0;
+        int diamondCount = 0;
+        int heartCount = 0;
+
+        for (Card card : allCard) {
+            Integer cardOrder = denominationList.indexOf(card.getDenomination());
+
+            if (card.getSuit() == Card.Suit.DIAMOND) {
+                diamondCount += 1;
+                diamondCards.add(cardOrder);
+            } else if (card.getSuit() == Card.Suit.CLOVER) {
+                cloverCount += 1;
+                cloverCards.add(cardOrder);
+            } else if (card.getSuit() == Card.Suit.SPADE) {
+                spadeCount += 1;
+                spadeCards.add(cardOrder);
+            } else {
+                heartCount += 1;
+                heartCards.add(cardOrder);
+            }
+        }
+
+        if (diamondCount > 4) {
+            diamondCards.sort(Comparator.naturalOrder());
+
+            return "DIAMOND";
+        }
+        if (cloverCount > 4) {
+            cloverCards.sort(Comparator.naturalOrder());
+
+            return "clover";
+        } if (spadeCount > 4) {
+            spadeCards.sort(Comparator.naturalOrder());
+
+            return "spade";
+        } if (heartCount > 4) {
+            heartCards.sort(Comparator.naturalOrder());
+
+            return "heart";
+        }
+
+        return "nope";
+    }
+
 
     private int findStraight(List<Card> allCard) {
         List<String> denominationList = Card.getDenominationList();
